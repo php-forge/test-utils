2496cedbdfff4e85b8e5be6d38466b52a9d58d15
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-05-30 11:48:50 -0400

    Compromiso inicial. (#1)
    * Compromiso inicial.

---
+++ src/Assert.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Forge\TestUtils;
+
+use PHPUnit\Framework\TestCase;
+
+use function str_replace;
+
+/**
+ * @psalm-suppress PropertyNotSetInConstructor
+ */
+final class Assert extends TestCase
+{
+    /**
+     * Asserting two strings equality ignoring line endings.
+     *
+     * @param string $expected
+     * @param string $actual
+     * @param string $message
+     */
+    public function equalsWithoutLE(string $expected, string $actual, string $message = ''): void
+    {
+        $expected = str_replace("\r\n", "\n", $expected);
+        $actual = str_replace("\r\n", "\n", $actual);
+        $this->assertEquals($expected, $actual, $message);
+    }
+}
